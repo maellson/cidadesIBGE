b353c0b4a23afb0d7ff464f4e9aa59b4a2a6af78
maellson <mqmaellson39@gmail.com>
2023-06-06 00:59:31 -0300

    MODIFICADO PARA TRATAR OS ERROS DE BUSCA NAS ROTAS

--- routes.js
+++ routes.js
@@ -8,31 +8,47 @@ const router = express.Router();
 
 // Rota que retorna as cidades correspondentes ao estado e nome da cidade
 router.get('/cidades/:cod_uf/:nome_municipio', async (req, res) => {
-    const { cod_uf, nome_municipio } = req.params;
+    try {
+        const { cod_uf, nome_municipio } = req.params;
 
-    const cidades = await Cidade.findAll({
-        attributes: ['codigo_municipio', 'cod_uf', 'nome_municipio'],
-        where: {
-            cod_uf,
-            nome_municipio: {
-                [Sequelize.Op.iLike]: `${nome_municipio}%`
+        const cidades = await Cidade.findAll({
+            attributes: ['codigo_municipio', 'cod_uf', 'nome_municipio'],
+            where: {
+                cod_uf,
+                nome_municipio: {
+                    [Sequelize.Op.iLike]: `${nome_municipio}%`
+                }
             }
-        }
-    });
+        });
 
-    res.json(cidades);
+        if (cidades.length === 0) {
+            throw new Error('Cidade não encontrada');
+        }
+
+        res.json(cidades);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 // Rota que retorna todas as cidades do estado
 router.get('/cidades/:cod_uf', async (req, res) => {
-    const { cod_uf } = req.params;
+    try {
+        const { cod_uf } = req.params;
 
-    const cidades = await Cidade.findAll({
-        attributes: ['codigo_municipio', 'cod_uf', 'nome_municipio'],
-        where: { cod_uf }
-    });
+        const cidades = await Cidade.findAll({
+            attributes: ['codigo_municipio', 'cod_uf', 'nome_municipio'],
+            where: { cod_uf }
+        });
 
-    res.json(cidades);
+        if (cidades.length === 0) {
+            throw new Error('Cidade não encontrada');
+        }
+
+        res.json(cidades);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 module.exports = router;
